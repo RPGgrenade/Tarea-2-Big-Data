4d2e2e168de6e7d1d51a0693a03b07def6ecf37a
Jaime Echeverría <rpggrenade@gmail.com>
2017-10-10 15:09:18 -0300

    p4 completed

--- users_most_similar_ratings.py
+++ users_most_similar_ratings.py
@@ -1,5 +1,7 @@
 #-- coding: utf-8 --
 import time
+
+import math
 from mrjob.job import MRJob
 from mrjob.step import MRStep
 from mrjob.protocol import JSONValueProtocol
@@ -12,41 +14,53 @@ import json
 
 class SimilarUsersRatings(MRJob):
 
-    def mapper_user_ids(self, _, line):
+    def mapper_user_data(self, _, line):
         obj = json.loads(line)
         line = obj
         user_id = line['user_id']
         business_id = line['business_id']
-        yield user_id, business_id
+        rating = line['stars']/5
+        yield business_id, [user_id, rating]
 
-    def reducer_reviews_per_user(self, user_id, business_id):
-        business_list = [br for br in business_id]
-        review_count = len(business_list)
+    def reducer_user_pairs(self, business_id, user_ratings):
+        user_rating_list = [ur for ur in user_ratings]
+        #review_count = len(user_rating_list)
 
-        for business_id in business_list:
-            yield business_id, tuple([user_id, review_count])
+        for combination in itertools.combinations(user_rating_list,2):
+            yield business_id, sorted(combination)
 
-    def reducer_similarity(self, business_id, user_count):
-        user_count_list = [u for u in user_count]
-        for combination in itertools.combinations(user_count_list,2): #each pair of users
-            yield combination, 1
+    def reducer_pair_ratings(self, business, user_count_pairs):
+        #user_count_list = [u for u in user_count]
 
-    def reducer_jaccard(self, user_pair, value):
-        pair_list = [p for p in user_pair]
-        jaccard = sum(value) * 1.0 / ((pair_list[0][1] + pair_list[1][1]) - sum(value))
-        yield [pair_list[0][0], pair_list[1][0]], jaccard # sum(values) * 1.0 / (key[0][1] + key[1][1] - sum(values))
+        for pair in user_count_pairs:
+          user_a = pair[0][0]
+          user_b = pair[1][0]
+          rating_a = pair[0][1]
+          rating_b = pair[1][1]
+          users = sorted([user_a, user_b])
+          ratings = [rating_a, rating_b]
+          yield users, ratings
 
-    def reducer_similar_pairs(self, key, values):
-        similarity = sum(values)
-        if similarity >= 0.5:
-            yield "Pair:", [key[0],key[1]]
+    def reducer_jaccard(self, user_pair, rating_pairs):
+
+        total_dividend = 0
+        total_divider_a = 0
+        total_divider_b = 0
+        for pair in rating_pairs:
+            rating_a = pair[0]
+            rating_b = pair[1]
+            total_dividend += (rating_a*rating_b)
+            total_divider_a += (rating_a*rating_a)
+            total_divider_b += (rating_b*rating_b)
+        similarity = total_dividend * 1.0/(math.sqrt(total_divider_a)*math.sqrt(total_divider_b))
+        if similarity >= 1.0:
+            yield "Pair", user_pair
 
     def steps(self):
-        return [MRStep(mapper=self.mapper_user_ids),
-                MRStep(reducer=self.reducer_reviews_per_user),
-                MRStep(reducer=self.reducer_similarity),
-                MRStep(reducer=self.reducer_jaccard),
-                MRStep(reducer=self.reducer_similar_pairs)]
+        return [MRStep(mapper=self.mapper_user_data),
+                MRStep(reducer=self.reducer_user_pairs),
+                MRStep(reducer=self.reducer_pair_ratings),
+                MRStep(reducer=self.reducer_jaccard)]
 
 
 if __name__ == '__main__':
